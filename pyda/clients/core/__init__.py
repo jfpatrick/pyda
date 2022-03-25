5a49fadc6d594ff57ffb5fccb179282b28a9858b
Ivan Sinkarenko <ivan.sinkarenko@cern.ch>
2022-03-25 14:57:54 +0100

    Port Client implementations from pyda-toy-model

---
+++ pyda/clients/core/__init__.py
@@ -0,0 +1,5 @@
+from ._core import BaseClient, BaseSubscription, BaseSubscriptionPool
+
+BaseClient.__module__ = __name__
+BaseSubscription.__module__ = __name__
+BaseSubscriptionPool.__module__ = __name__
