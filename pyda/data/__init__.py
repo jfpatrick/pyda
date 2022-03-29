6f673f63bee778ee1ece05bb2061c8a226e88e65
Ivan Sinkarenko <ivan.sinkarenko@cern.ch>
2022-03-29 12:29:56 +0200

    Add data types for the SET interaction (wihtout tests yet)

--- pyda/data/__init__.py
+++ pyda/data/__init__.py
@@ -4,6 +4,8 @@ from ._data import (
     PropertyAccessError,
     PropertyAccessQuery,
     PropertyAccessResponse,
+    PropertyUpdateResponse,
+    UpdateHeader,
     Selector,
 )
 
@@ -11,5 +13,7 @@ AcquiredPropertyData.__module__ = __name__
 PropertyAccessError.__module__ = __name__
 PropertyAccessQuery.__module__ = __name__
 PropertyAccessResponse.__module__ = __name__
+PropertyUpdateResponse.__module__ = __name__
+UpdateHeader.__module__ = __name__
 Header.__module__ = __name__
 Selector.__module__ = __name__
